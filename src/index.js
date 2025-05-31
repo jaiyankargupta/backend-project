30bd3cd3befff77ff2b2d459a20283ae180ed974
jaiyankargupta <jaiyankargupta@gmail.com>
2025-05-31 21:44:55 +0530

    connecting DB

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import monngoos from "mongoos";
-import { DB_NAME } from "./constant";
-import express from "express";
+// import monngoos from "mongoos";
+// import { DB_NAME } from "./constant";
+// import express from "express";
 
-const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-async () => {
-  try {
-    await monngoos.connect(`${process.env.MONDODB_URI}/${DB_NAME}`);
-    app.on("error", () => {
-      console.log("error", error);
-    });
-    app.listen(process.env.PORT, () => {
-      console.log("listening...", process.env.PORT);
-    });
-  } catch (error) {
-    console.log("Error: ", error);
-    throw error;
-  }
-};
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// const app = express();
+
+// async () => {
+//   try {
+//     await monngoos.connect(`${process.env.MONDODB_URI}/${DB_NAME}`);
+//     app.on("error", () => {
+//       console.log("error", error);
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log("listening...", process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error);
+//     throw error;
+//   }
+// };
